9aec92f082b2bf4b416ba43468889e4ffa56c7d1
Shakil Hossain <shakilhossainp.cse@gmail.com>
2021-08-10 17:25:34 +0600

    setup react router dom and create admin dashboard layout

---
+++ src/layouts/admin/masterLayout.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import '../../assets/admin/css/styles.css';
+import '../../assets/admin/js/scripts';
+
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+
+export default function MasterLayout() {
+  return (
+    <div className="sb-nav-fixed">
+      <Navbar />
+      <div id="layoutSidenav">
+
+        <div id="layoutSidenav_nav">
+            <Sidebar/>
+        </div>
+
+        <div id="layoutSidenav_content">
+            <main>
+                Master File 
+            </main>
+            <Footer/>
+        </div>
+
+      </div>
+    </div>
+  );
+}
